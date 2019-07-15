c42147dc59e59f89719c5b92ef6e5467453a9382
Travis Jeffery <tj@travisjeffery.com>
2019-07-15 17:37:17 -0400

    style fixes

--- proced-narrow.el
+++ proced-narrow.el
@@ -1,4 +1,5 @@
-;;; proced-narrow.el --- Live-narrowing of search results for proced.
+;;; proced-narrow.el --- Live-narrowing of search results for proced. -*- lexical-binding: t; -*-
+
 
 ;; Copyright (C) 2019 Travis Jeffery
 
@@ -9,6 +10,25 @@
 ;; Created: 15th July 2019
 ;; Version: 1.0.0
 ;; Package-requires: ((dash "2.7.0") (proced))
+
+;; This file is NOT part of GNU Emacs.
+
+;;; License:
+
+;; This program is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation; either version 3, or (at your option)
+;; any later version.
+;;
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+;;
+;; You should have received a copy of the GNU General Public License
+;; along with GNU Emacs; see the file COPYING.  If not, write to the
+;; Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+;; Boston, MA 02110-1301, USA.
 
 ;;; Commentary:
 
@@ -37,6 +57,7 @@
     map)
   "Keymap used while `proced-narrow' is reading the pattern.")
 
+;;;###autoload
 (defun proced-narrow ()
   "Narrow a proced buffer to the processes matching a string.
 
